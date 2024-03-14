fb5f8293ed8b1ae919a63e664d24bc07a6695093
ReshmaCR86 <156369914+ReshmaCR86@users.noreply.github.com>
2024-03-15 02:23:14 +0530

    git add manage.py

---
+++ Reminder_project/remainder_app/views.py
@@ -0,0 +1,14 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ReminderSerializer
+
+
+@api_view(["POST"])
+def create_reminder(request):
+    serializer = ReminderSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
